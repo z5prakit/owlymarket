f1b1e57333587a3e140553be956fa9239cfbf7fe
jirassssa <pppikapika1@gmail.com>
2025-12-31 15:32:43 +0700

    Fix: Force dynamic rendering for pages using Privy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { Container } from '@/components/layout/Container'
